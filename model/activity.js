38e13dd51b6b1cb71fa6a09fb58234a54e387033
heath smith <heathsmith73@gmail.com>
2018-02-13 13:37:41 -0800

    all routes and admin duties working

--- model/activity.js
+++ model/activity.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const Activity = mongoose.Schema({
   name: {type: String, required: true},
   location: {type: String, required: true},
+  display: false,
   leaderBoard: [],
   users: [],
 });
